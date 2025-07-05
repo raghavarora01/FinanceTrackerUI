8537ee72fefa91cdbd57be751177c7e3534e62d1
Raghav Arora <97450013+raghavarora01@users.noreply.github.com>
2025-07-06 01:59:21 +0530

    Update budget.ts

--- src/api/budget.ts
+++ src/api/budget.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://financetrackerapi-6lik.onrender.com';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://financetrackerapi-production.up.railway.app/';
 const FLASK_API_URL = process.env.NEXT_PUBLIC_FLASK_API || 'https://financetrackerpyhtonapi-1.onrender.com';
 
 function getAuthHeaders() {
